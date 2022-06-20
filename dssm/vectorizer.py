b1e9bed47efda06fc6aeaa47d708ef1f4d909704
Shan Dou <shan.dou@loblaw.ca>
2022-06-20 23:13:25 +0000

    Add nltk punkt downloader

--- dssm/vectorizer.py
+++ dssm/vectorizer.py
@@ -1,21 +1,27 @@
 # -*- coding: utf-8 -*-
 from .utils import tokenize4zh
 from sklearn.feature_extraction.text import CountVectorizer
+import nltk
 from nltk.tokenize import word_tokenize
 import pickle
 
 
 class Vectorizer(CountVectorizer):
-
     def __init__(self, lang=None, **kwargs):
         self.lang = lang
         super(Vectorizer, self).__init__(**kwargs)
 
     def preprocess(self, raw_documents):
-        if self.lang == 'en':
-            raw_documents = [' '.join(word_tokenize(raw_document)) for raw_document in raw_documents]
-        elif self.lang == 'zh':
-            raw_documents = [' '.join(tokenize4zh(raw_document)) for raw_document in raw_documents]
+        if self.lang == "en":
+            try:
+                word_tokenize(raw_documents[0])
+            except LookupError:
+                nltk.download("punkt")
+            raw_documents = [
+                " ".join(word_tokenize(raw_document)) for raw_document in raw_documents
+            ]
+        elif self.lang == "zh":
+            raw_documents = [" ".join(tokenize4zh(raw_document)) for raw_document in raw_documents]
         return raw_documents
 
     def fit(self, raw_documents):
@@ -27,7 +33,7 @@ class Vectorizer(CountVectorizer):
         return super(Vectorizer, self).transform(raw_documents)
 
     def partial_fit(self, raw_documents):
-        if(hasattr(self, 'vocabulary_')):
+        if hasattr(self, "vocabulary_"):
             vocab = self.vocabulary_
         else:
             vocab = {}
@@ -38,9 +44,9 @@ class Vectorizer(CountVectorizer):
         self.vocabulary_ = {vocab[i]: i for i in range(len(vocab))}
 
     def load(self, path):
-        with open(path, 'rb') as f:
+        with open(path, "rb") as f:
             self.vocabulary_ = pickle.load(f)
 
     def save(self, path):
-        with open(path, 'wb') as f:
+        with open(path, "wb") as f:
             pickle.dump(self.vocabulary_, f)
